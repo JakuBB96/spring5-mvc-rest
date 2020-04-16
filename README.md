4dfaa593e3d2449d1a76bc4088b26a3319666683
JakuBB96 <JakuBB96@javadev.com>
2020-04-16 13:27:06 +0200

    Implemented MapStruct, closes #4, Added status badges closes #5

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+[![CircleCI](
+https://circleci.com/gh/JakuBB96/spring5-mvc-rest.svg?style=svg)](https://circleci.com/gh/JakuBB96/spring5-mvc-rest)
+
+[![codecov](
+https://codecov.io/gh/JakuBB96/spring5-mvc-rest/branch/master/graph/badge.svg)](https://codecov.io/gh/JakuBB96/spring5-mvc-rest)
+
 # spring5-mvc-rest
  Restful Interfaces with Spring
---
+++ src/test/java/com/barancewicz/api/v1/mapper/CategoryMapperTest.java
@@ -0,0 +1,27 @@
+package com.barancewicz.api.v1.mapper;
+
+import com.barancewicz.api.v1.model.CategoryDTO;
+import com.barancewicz.domain.Category;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryMapperTest {
+
+
+    public static final String NAME = "testCat";
+    public static final long ID = 1L;
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+    @Test
+    void categoryToCategoryDTO() {
+
+        Category category = new Category();
+        category.setId(ID);
+        category.setName(NAME);
+
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+        assertEquals(Long.valueOf(ID), categoryDTO.getId());
+        assertEquals(NAME, categoryDTO.getName());
+    }
+}
