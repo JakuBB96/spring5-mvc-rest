e2fceeb6c6c9165dea6a1816b345ca4527f1573d
JakuBB96 <JakuBB96@javadev.com>
2020-04-17 17:35:54 +0200

    Created simple documentation using SWAGGER

--- src/main/java/com/barancewicz/api/v1/model/CustomerDTO.java
+++ src/main/java/com/barancewicz/api/v1/model/CustomerDTO.java
@@ -1,12 +1,15 @@
 package com.barancewicz.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
     private Long id;
+    @ApiModelProperty(value = "This is first name", required = true)
     private String firstname;
+    @ApiModelProperty(required = true)
     private String lastname;
     @JsonProperty("customer_url")
     private String customerUrl;
